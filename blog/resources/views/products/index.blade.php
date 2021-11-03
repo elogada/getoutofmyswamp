b9fdb23cb4edfa46e3ef5a7925abb0ffcb2d96a8
root <bayanielogada@gmail.com>
2021-11-03 22:59:06 +0000

    update new vue components

--- blog/resources/views/products/index.blade.php
+++ blog/resources/views/products/index.blade.php
@@ -1,40 +1,23 @@
 @extends('products.layout') 
 @section('content')
-
-<div>
-<main-title></main-title>
+<table class="table table-bordered"> 
 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
-</div>
 @if ($message = Session::get('success'))
 <div class="alert alert-success">
 <p>{{ $message }}</p>
 </div>
 @endif
-  
-<table class="table table-bordered">
-<tr>
-<th>No</th>
-<th>Name</th>
-<th>Details</th>
-<th width="280px">Action</th>
-</tr>
+<labels-component></labels-component>
 @foreach ($products as $product)
-<tr>
-<td>{{ ++$i }}</td>
+<tr><td>{{ ++$i }}</td>
 <td>{{ $product->name }}</td>
-<td>{{ $product->detail }}</td>
-<td>
+<td>{{ $product->detail }}</td><td>
 <form action="{{ route('products.destroy',$product->id) }}" method="POST">									   
 <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>    
 <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
-  
 @csrf
 @method('DELETE')
-   
-<button type="submit" class="btn btn-danger">Delete</button>
-</form>
-</td>
-</tr>
+<closer-component></closer-component>
 @endforeach
 </table>									  
 {!! $products->links() !!}
