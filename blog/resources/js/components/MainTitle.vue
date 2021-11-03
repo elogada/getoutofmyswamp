5abdcd9852dec609b39935771eac397fcbba7a33
root <bayanielogada@gmail.com>
2021-11-03 22:28:42 +0000

    new components

---
+++ blog/resources/js/components/MainTitle.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="row">
+<div class="col-lg-12 margin-tb">
+<div class="pull-left">
+<h2>Bayani's Peripherals List</h2>
+</div>
+<div class="pull-right">
+<a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+</div>
+</div>
+</div>
+</template>
