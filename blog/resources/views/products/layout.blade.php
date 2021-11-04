b3e0a4dc7ec9b8fe8f0063eae2f869e1b2ecb43f
root <bayanielogada@gmail.com>
2021-11-04 00:07:38 +0000

    initiate tailwind

--- blog/resources/views/products/layout.blade.php
+++ blog/resources/views/products/layout.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, intial-scale=1.0" />
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <title>Bayani's DB</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 <script src="{{ asset('js/app.js') }}" defer></script>
